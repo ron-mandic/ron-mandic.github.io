dd3c68c17efed07438e0d25ec26576bfec773b50
ron-mandic <ron.mandic@hfg.design>
2022-11-08 10:42:59 +0100

    0x000 - Initialization

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let direction = 0;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	fill("white");
+	textAlign(CENTER, CENTER);
+	textSize(700);
+}
+
+function draw() {
+	const angleInDegrees = direction * 45;
+	const angleInRadians = radians(angleInDegrees);
+
+	background("black");
+
+	push();
+	translate(width / 2, height / 2);
+	rotate(angleInRadians);
+	text("↑", 0, 0);
+	pop();
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseReleased() {
+	direction = round(random(0, 7));
+}
